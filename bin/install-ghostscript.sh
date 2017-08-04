cf25ab8138d927b47435358510cb9588f7668087
gitlost <burmartke@gmail.com>
2017-08-04 17:51:16 +0100

    Return true on ! if in install-ghostscript.sh.

--- bin/install-ghostscript.sh
+++ bin/install-ghostscript.sh
@@ -2,9 +2,8 @@
 
 set -ex
 
-if [[ $TRAVIS_SUDO ]]; then
-	sudo apt-get -qq update
-	sudo apt-get install -y ghostscript
-else
-	true
+if [[ ! $TRAVIS_SUDO ]]; then
+	return true
 fi
+sudo apt-get -qq update
+sudo apt-get install -y ghostscript
